676ce0737d8c8f4103b87fd6eec3794be04c6b28
Leigh <psychotropes661@gmail.com>
2025-03-12 15:50:16 +0100

    ⚡️(perf) Validation avec Zod

--- gamerz-front/src/components/Register.tsx
+++ gamerz-front/src/components/Register.tsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom"
 import { SubmitHandler, useForm } from 'react-hook-form'
-import { User } from "../models/UserModel"
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
 
+
+const schema = z.object({
+    email: z.string().email(),
+    pseudo: z.string().min(1, "How should we call you ?"), 
+    candidacyText: z.string().min(50, "We would like to know more ... "),
+    password: z.string()
+      .min(8, "Password must have at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character")
+  });
+
+type FormFields = z.infer<typeof schema>
 
 function Register() {
+
     const {
         register,
         handleSubmit,
         setError,
         formState: { errors, isSubmitting }
-    } = useForm<User>();
+    } = useForm<FormFields>({
+        resolver: zodResolver(schema)
+    });
 
-    const onSubmit: SubmitHandler<User> = async (data) => {
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
         try {
             await new Promise((resolve) => setTimeout(resolve, 1000))
             throw new Error()
@@ -28,8 +45,6 @@ function Register() {
         <>
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 
-
-
                 <form className="fieldset flex flex-col items-center w-lg bg-base-200 border border-base-300 p-4 rounded-box" onSubmit={handleSubmit(onSubmit)}>
                     <legend className="fieldset-legend text-2xl">Register</legend>
 
@@ -37,64 +52,25 @@ function Register() {
 
                     {errors.email && <div className="text-error text-xs font-extralight italic"> {errors.email.message} </div>}
 
-                    <input {...register("email", {
-                        required: "Email is required",
-                        validate: (value) => {
-                            if (!value.includes("@")) {
-                                return "This is not an email"
-                            }
-                            return true;
-                        },
-                    })} type="email" className="input w-md" />
-
-
+                    <input {...register("email")} type="email" className="input w-md" />
 
                     <label className="fieldset-label text-base ml-9 text-left w-full">Password</label>
 
                     {errors.password && <div className="text-error text-xs font-extralight italic"> {errors.password.message} </div>}
 
-                    <input {...register("password", {
-                        required: "Password is required",
-                        minLength: {
-                            value: 8,
-                            message: "Password must have at least 8 characters"
-                        },
-                        validate: (value) => {
-                            const hasUpperCase = /[A-Z]/.test(value);
-                            const hasLowerCase = /[a-z]/.test(value);
-                            const hasNumber = /[0-9]/.test(value);
-                            const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
-                      
-                            if (!hasUpperCase) return "Password must contain at least one uppercase letter";
-                            if (!hasLowerCase) return "Password must contain at least one lowercase letter";
-                            if (!hasNumber) return "Password must contain at least one number";
-                            if (!hasSpecialChar) return "Password must contain at least one special character";
-                      
-                            return true;
-                          },
-                    })} type="password" className="input w-md" />
-
-
+                    <input {...register("password")} type="password" className="input w-md" />
 
                     <label className="fieldset-label text-base ml-9 text-left w-full">Pseudo</label>
 
                     {errors.pseudo && <div className="text-error text-xs font-extralight italic"> {errors.pseudo.message} </div>}
 
-                    <input {...register("pseudo", {
-                        required: "How should we call you ?"
-                    })} type="text" className="input w-md" />
-
-
+                    <input {...register("pseudo")} type="text" className="input w-md" />
 
                     <label className="fieldset-label text-base ml-9 text-left w-full">Tell us why you want to join :</label>
 
                     {errors.candidacyText && <div className="text-error text-xs font-extralight italic"> {errors.candidacyText.message} </div>}
 
-                    <textarea {...register("candidacyText", {
-                        required: "We need to know your motivation to join"
-                    })} className="textarea w-md" />
-
-
+                    <textarea {...register("candidacyText")} className="textarea w-md" />
 
                     <button disabled={isSubmitting} type="submit" className="btn btn-soft btn-primary text-base mt-4">
                         {isSubmitting ? "Let him cook..." : "Submit"}
