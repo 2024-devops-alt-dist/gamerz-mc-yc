20d02e75a65218f5cd3531c2fa1a921626cd578c
Leigh <psychotropes661@gmail.com>
2025-04-10 10:03:15 +0200

    ✨ (feat) - BACK: Implement JWT authentication middleware and user role checks for Admin routes

--- gamerz-back/src/routes/chatroomRouter.ts
+++ gamerz-back/src/routes/chatroomRouter.ts
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import {chatroomController} from "../controller/chatroomController";
+import { checkToken, isAdmin } from "../middlewares/autorization";
 
 export const chatRoomRouter = Router()
 
 // @ts-ignore
-chatRoomRouter.post('/new-chatroom', chatroomController.create)
+chatRoomRouter.post('/new-chatroom', checkToken, isAdmin, chatroomController.create)
